d53ea03234c192981b7881457dded7dc910b1046
RicardoDanta <uruguaydenos@gmail.com>
2022-12-07 07:48:26 -0800

    Change things!

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,12 +2,12 @@
 """Python script that for a given employee ID
 returns information about his/her TODO list progress"""
 
-import request
+import requests
 
 if __name__ == '__main__':
     url = "https://jsonplaceholder.typicode.com/todos/1"
-    todos = request.get(url)
+    todos = requests.get(url)
     if todos.status_code == 200:
         todos = todos.json()
-        for i in todos["todos"]:
+        for i in todos:
             print(i["title"])
