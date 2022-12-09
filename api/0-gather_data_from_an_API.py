12202bba1bf214ffb72ed7ac11196bd3016c4b6e
RicardoDanta <uruguaydenos@gmail.com>
2022-12-08 21:16:52 -0800

    Change code!

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,13 +1,33 @@
 #!/usr/bin/python3
-"""Python script that for a given employee ID
-returns information about his/her TODO list progress"""
+"""Write a Python script that, using this REST API
+for a given employee ID
+returns information about his/her TODO list progress
+Code by Mauricio Miranda"""
 
+import json
 import requests
+from sys import argv
 
-if __name__ == '__main__':
-    url = "https://jsonplaceholder.typicode.com/todos/1"
-    todos = requests.get(url)
-    if todos.status_code == 200:
-        todos = todos.json()
-        for i in todos:
-            print(i["title"])
+
+if __name__ == "__main__":
+
+    get__user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                             .format(argv[1]))
+    get__name = (get__user.json().get("name"))
+
+    var = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
+                       .format(argv[1]))
+
+    get__val = var.json()
+    get__len__val = len(get__val)
+    res = []
+    count = 0
+    for task in get__val:
+        if task["completed"] is True:
+            res.append(task.get("title"))
+            count += 1
+    print("Employee {} is done with tasks({}/{}):"
+          .format(get__name, count, get__len__val))
+
+    for title in res:
+        print("\t {}".format(title))
