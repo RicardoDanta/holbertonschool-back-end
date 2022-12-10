4c6a599b750c9f515c811d66d0c40c34d47aca59
RicardoDanta <uruguaydenos@gmail.com>
2022-12-09 18:38:42 -0800

    Change the code again!

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,31 +1,15 @@
 #!/usr/bin/python3
 """Write a Python script that, using this REST API
 for a given employee ID
-returns information about his/her TODO list progress
-Code by Mauricio Miranda"""
+returns information about his/her TODO list progress"""
 
 import json
 import requests
-from sys import argv
 
 
-if __name__ == "__main__":
-    """Script that returns information about user todo list"""
-    url = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos".
-            format(argv[1])).json()
-    user_name = requests.get("https://jsonplaceholder.typicode.com/users/{}".
-            format(argv[1])).json()
-    completed_tasks = 0
-    uncompleted_tasks = 0
-    for i in url:
-        if i["completed"] is True:
-            completed_tasks = completed_tasks + 1
-    for i in url:
-        if i["completed"] is False:
-            uncompleted_tasks = uncompleted_tasks + 1
-    print("Employee {} is done with tasks ({}/{}):".
-          format(user_name["name"], completed_tasks,
-                 (completed_tasks + uncompleted_tasks)))
-    for i in url:
-        if i["completed"] is True:
-            print("\t {}".format(url["title"]))
+url = ("https://jsonplaceholder.typicode.com/todos/")
+data = requests(url)
+if data.status_code == 200:
+    data = data.json()
+    for i in data:
+        print(i["title"])
