b2bf34047cf795cff94b19b760fc98b3c811d65f
RicardoDanta <uruguaydenos@gmail.com>
2022-12-09 18:47:37 -0800

    Change code again!

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,13 +3,25 @@
 for a given employee ID
 returns information about his/her TODO list progress"""
 
-import json
 import requests
+import json
+from sys import argv
 
 
-url = ("https://jsonplaceholder.typicode.com/todos/")
-data = requests(url)
-if data.status_code == 200:
-    data = data.json()
-    for i in data:
-        print(i["title"])
+if __name__ == '__main__':
+    url = requests.get("https://jsonplaceholder.typicode.com/users/" +
+                       argv[1])
+    todos = json.loads(url.text)
+    name = todos.get("name")
+    url = requests.get("https://jsonplaceholder.typicode.com/todos/" +
+                       "?userId=" + argv[1])
+
+    todos_list = json.loads(url.text)
+    tasks = len(todos_list)
+    done = [task for task in todos_list if task.get("completed")]
+    completed_tasks = len(done)
+    print("Employee {} is done with tasks({}/{}):".
+          format(name, completed_tasks, tasks))
+
+    for task in done:
+        print("\t", task.get('title'))
